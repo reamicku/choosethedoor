10b75ab81f5addb68806efe31a13e7f0ad99eeef
reamicku <rmku@proton.me>
2024-06-05 19:49:20 +0200

    Rebuild main script

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from neurality import NeuralNet
+from neurality import NeuralNet, Distribution, ActivationFn
+from neurality.utils import generate_non_overlapping_pairs
 from ctdsim.elements import *
 import copy
 from tqdm import tqdm
@@ -6,37 +7,59 @@ import time
 import os
 import shutil
 
-saveNetworkImages = True
+
+
+##### Define values #####
+### Simulation variables
+n_rooms = 10
+n_trapdoors = 0
+n_fakedoors = 8
+n_creatures = 1000
+
+### Simulation variables cont.
+n_neuralnet_processing_steps = 8 # Neural net gets updated N times before making a decision
+n_newnet_creatures_step = 10 # Create new nn every N creatures
+
+### Neural Network
+n_internal_neurons = 5
+n_connections = 80
+forbid_direct_io_connections = True
+activation_fn = ActivationFn.LEAKY_RELU
+random_distrib = Distribution.HE
+random_distrib_range = [-4.0, 4.0]
+save_network_images = True
+#########################
+
+
+n_alldoors = n_trapdoors + n_fakedoors + 1
 
 sim = Simulation()
 
 # Create a simulation with N rooms
-for i in range(0, 10):
-    sim.addRoom(0, 4)
-
+for i in range(0, n_rooms):
+    sim.addRoom(n_trapdoors, n_fakedoors)
 
 # Create N creatures
-for i in tqdm(range(0, 10000), desc='Creating creatures'):
-    creature = Creature(5, 5)
+for i in tqdm(range(0, n_creatures), desc='Creating creatures'):
+    creature = Creature(n_alldoors, n_alldoors)
 
-    if i % 1000 == 0:
+    if i % n_newnet_creatures_step == 0:
         while True:
-            nn = NeuralNet(5, 5, 5, (5+5+5)**1.3, forbidDirectIOConnections=True)
+            nn = NeuralNet(n_alldoors, n_alldoors, n_internal_neurons, n_connections, activation_fn=activation_fn, forbidDirectIOConnections=forbid_direct_io_connections)
             if nn.isAllInputOutputConnected():
                 break
-
     newnn = copy.deepcopy(nn)
-    newnn.mutate(1.0)
+    newnn.mutate(1.0, distrib=random_distrib, lower=random_distrib_range[0], upper=random_distrib_range[1])
     creature.setNeuralNetwork(newnn)
     sim.addCreature(creature)
 
 # Warmup
-for i in tqdm(range(0,7), desc='Simulation warmup'):
+for i in tqdm(range(0,n_neuralnet_processing_steps), desc='Simulation warmup'):
     sim.step()
 
 # Simulate
-for i in tqdm(range(0, 77+1), desc='Simulating'):
-    chooseDoor = (i%7==0)
+for i in tqdm(range(0, n_rooms*n_neuralnet_processing_steps+1), desc='Simulating'):
+    chooseDoor = (i%n_neuralnet_processing_steps==0)
     if i==0:
         chooseDoor = False
     sim.step(chooseDoor=chooseDoor)
@@ -58,8 +81,8 @@ bestCreatures = sim.getBestCreatureElements()
 
 if len(bestCreatures) < 50:
     for i, el in enumerate(bestCreatures):
-        if saveNetworkImages:
-            if el['creature'].nn.getAllNeuronCount() <= 64:
+        if save_network_images:
+            if el['creature'].nn.getAllNeuronCount() <= 30:
                 f.write(f'\n\n![](./imgs/bestnetwork{i}.png)')
                 el['creature'].nn.saveNetworkImage(f'output/simulation/imgs/bestnetwork{i}')
 f.close()
